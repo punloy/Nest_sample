34214acf435bd9a5192584e5fbb8eec45e087855
heatherww <pieda0123@yahoo.com.tw>
2021-03-21 22:54:45 +0800

    add health check

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from './di/AppModule';
 
-async function bootstrap() {
+function applySwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle("Twitter sample")
+    .setDescription("The Twitter sample API description")
+    .setVersion("0.1")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api-docs", app, document);
+}
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  applySwagger(app);
   await app.listen(3000);
 }
 bootstrap();
