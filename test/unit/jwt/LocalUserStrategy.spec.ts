d6d0ab5bb808b602477e4ded1695d6997c6d9af5
heatherww <pieda0123@yahoo.com.tw>
2021-03-24 00:58:30 +0800

    add unit test

---
+++ test/unit/jwt/LocalUserStrategy.spec.ts
@@ -0,0 +1,43 @@
+import { Test } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { tweet } from "../../../src/core";
+import { LocalUsertrategy } from "../../../src/core/jwt/LocalUserStrategy";
+import { encodeBase64 } from "../../../src/core/utils/encodeBase64";
+import { TweetTestModule } from "../../../src/di/TweetTestModule";
+import { AuthService } from "../../../src/service/AuthService";
+import { createTestUser } from "../../../test/helper/TestTweetData";
+
+
+describe("LocalCustomerAccountStrategy spec", () => {
+    let localUsertrategy: LocalUsertrategy;
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [
+                TweetTestModule.register(),
+                TypeOrmModule.forFeature(Object.values(tweet))
+            ],
+            providers: [
+                AuthService,
+                LocalUsertrategy
+            ]
+        }).compile();
+
+        localUsertrategy = moduleRef.get<LocalUsertrategy>(LocalUsertrategy);
+
+        await createTestUser({ userId: 1 });
+    });
+
+    it("should validate", async () => {
+        const localUserStrategy = await localUsertrategy.validate("test user 1", "password");
+        expect(localUserStrategy.id).toBe(1);
+        expect(localUserStrategy.name).toBe("test user 1");
+        //expect(localUserStrategy.passwordHash).toBe(encodeBase64("password"));
+    });
+
+    it("should not validate if account is invalid", async () => {
+        await expect(localUsertrategy.validate("test2", "password"))
+            .rejects.toThrowErrorMatchingSnapshot();
+    });
+
+});
